ac5431569af0222c7a0ec96ce37436ba6023e8a4
NamalTharindu <101260728+NamalTharindu@users.noreply.github.com>
2022-04-27 12:47:57 +0530

    first commit

---
+++ client/src/pizzadata.js
@@ -0,0 +1,135 @@
+const pizzas = [{
+    "name" : "PEPER BARBECUE CHICKEN",
+    "varients" : [
+        "small",
+        "mediam",
+        "large"
+    ],
+    "prices" : [
+        {
+            "small" : 200,
+            "mediam" : 350,
+            "large" : 400
+
+        }
+
+    ],
+
+    "category" : "nonveg",
+    "image" : "https://www.dominos.co.in/files/items/Chicken_Golden_Delight.jpg",
+    "description" : "Pepper Barbecue Chicken I Cheese"
+
+},
+{
+    "name" : "Non Veg Supereme",
+    "varients" : [
+        "small",
+        "mediam",
+        "large"
+    ],
+    "prices" : [
+        {
+            "small" : 200,
+            "mediam" : 350,
+            "large" : 400
+
+        }
+
+    ],
+
+    "category" : "nonveg",
+    "image" : "https://www.dominos.co.in/files/items/Farmhouse.jpg",
+    "description" : "Pepper Barbecue Chicken I Cheese"
+
+},
+{
+    "name" : "Golden Corn Pizza",
+    "varients" : [
+        "small",
+        "mediam",
+        "large"
+    ],
+    "prices" : [
+        {
+            "small" : 200,
+            "mediam" : 350,
+            "large" : 400
+
+        }
+
+    ],
+
+    "category" : "nonveg",
+    "image" : "https://www.dominos.co.in//files/items/golden_corn_veg.jpg",
+    "description" : "Pepper Barbecue Chicken I Cheese"
+
+},
+{
+    "name" : "Jalapeno & Red Paprika Pizza",
+    "varients" : [
+        "small",
+        "mediam",
+        "large"
+    ],
+    "prices" : [
+        {
+            "small" : 200,
+            "mediam" : 350,
+            "large" : 400
+
+        }
+
+    ],
+
+    "category" : "nonveg",
+    "image" : "https://www.dominos.co.in//files/items/Paneer_Makhni.jpg",
+    "description" : "Pepper Barbecue Chicken I Cheese"
+
+},
+{
+    "name" : "Margerita",
+    "varients" : [
+        "small",
+        "mediam",
+        "large"
+    ],
+    "prices" : [
+        {
+            "small" : 200,
+            "mediam" : 350,
+            "large" : 400
+
+        }
+
+    ],
+
+    "category" : "nonveg",
+    "image" : "https://www.dominos.co.in/files/items/Pepper_Barbeque.jpg",
+    "description" : "Pepper Barbecue Chicken I Cheese"
+
+},
+{
+    "name" : "Double Chease Margherita Pizza",
+    "varients" : [
+        "small",
+        "mediam",
+        "large"
+    ],
+    "prices" : [
+        {
+            "small" : 200,
+            "mediam" : 350,
+            "large" : 400
+
+        }
+
+    ],
+
+    "category" : "nonveg",
+    "image" : "https://www.dominos.co.in/files/items/Double_Cheese_Margherita.jpg",
+    "description" : "Pepper Barbecue Chicken I Cheese"
+
+},
+
+];
+export default pizzas;
